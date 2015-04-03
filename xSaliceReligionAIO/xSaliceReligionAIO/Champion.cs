4c1e094875600de0c1795addc5a937c1344af044
Matthias Markus Lakers <mothejeahk@googlemail.com>
2015-04-03 03:52:04 +0200

    Revert "DynamicActivator"
    This reverts commit bd63fbc3c0010d5ae694b7c6ae4280aa4ae6f75f.

--- xSaliceReligionAIO/xSaliceReligionAIO/Champion.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champion.cs
@@ -87,6 +87,33 @@ namespace xSaliceReligionAIO
         protected static Menu menu;
         private static readonly Menu OrbwalkerMenu = new Menu("Orbwalker", "Orbwalker");
 
+
+        private static TV NewInstance<TV>() where TV : class
+        {
+            return ObjectGenerator(typeof (TV)) as TV;
+        }
+
+        private static object NewInstance(Type type)
+        {
+            return ObjectGenerator(type);
+        }
+
+        private static object ObjectGenerator(Type type)
+        {
+            var target = type.GetConstructor(Type.EmptyTypes);
+            var dynamic = new DynamicMethod(string.Empty, type, new Type[0], target.DeclaringType);
+            var il = dynamic.GetILGenerator();
+            il.DeclareLocal(target.DeclaringType);
+            il.Emit(OpCodes.Newobj, target);
+            il.Emit(OpCodes.Stloc_0);
+            il.Emit(OpCodes.Ldloc_0);
+            il.Emit(OpCodes.Ret);
+
+            var method = (Func<object>) dynamic.CreateDelegate(typeof (Func<object>));
+            return method();
+        }
+    
+
         private void GameOnLoad()
         {
             Game.PrintChat("<font color = \"#FFB6C1\">xSalice's Religion AIO</font> by <font color = \"#00FFFF\">xSalice</font>");
@@ -118,9 +145,8 @@ namespace xSaliceReligionAIO
 
             try
             {
-                
                 var type = Type.GetType("xSaliceReligionAIO.Champions." + Player.ChampionName);
-                if (DynamicActivator.New(type, null) != null)
+                if (NewInstance(type) != null)
                 {
                     Game.PrintChat("<font color = \"#FFB6C1\">xSalice's " + Player.ChampionName + " Loaded!</font>");
                 }
